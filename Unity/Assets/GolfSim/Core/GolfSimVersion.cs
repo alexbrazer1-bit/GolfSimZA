a3983e47811720460c46f86cc28cf51592c4c580
alexbrazer1-bit <alex.brazer1@gmail.com>
2026-09-24 15:41:25 +0200

    Add simulator version marker

---
+++ Unity/Assets/GolfSim/Core/GolfSimVersion.cs
@@ -0,0 +1,8 @@
+namespace GolfSimZA.Core
+{
+    public static class GolfSimVersion
+    {
+        public const string Version = "0.1.0-dev";
+        public const string Codename = "R10 Foundation";
+    }
+}
